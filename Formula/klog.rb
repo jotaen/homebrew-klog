dee8ec32759336878e9545fbd5a40b358438cb91
Jan Heuermann <jan@jotaen.net>
2021-02-06 20:20:03 +0100

    Setup formula for v1.0

---
+++ Formula/klog.rb
@@ -0,0 +1,7 @@
+class Klog < Formula
+  desc "klog time tracking cli"
+  homepage "https://github.com/jotaen/klog"
+  url "https://github.com/jotaen/klog/releases/download/v1.0/klog-mac.zip"
+  sha256 "17795816e7cf715b4885ecac4c4cc35b4a5fefbfbad9f43c2ad0eb52ee078e57"
+  license "MIT"
+end
