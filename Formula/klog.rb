1e93571fc655ece32f23afb84f19e3a1a001cd8f
Jan Heuermann <jan@jotaen.net>
2021-10-17 20:54:21 +0200

    Add missing install section

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,7 +1,10 @@
 class Klog < Formula
-  desc "klog time tracking cli"
+  desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
   url "https://github.com/jotaen/klog/releases/download/v1.0/klog-mac.zip"
   sha256 "17795816e7cf715b4885ecac4c4cc35b4a5fefbfbad9f43c2ad0eb52ee078e57"
   license "MIT"
+  def install
+    bin.install "klog"
+  end
 end
