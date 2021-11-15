106a95989c4bb54561ad3c79e379c5e0656f44f5
Jan Heuermann <jan@jotaen.net>
2021-11-15 14:54:16 +0100

    Fix download link

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,7 +1,7 @@
 class Klog < Formula
   desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
-  url "https://github.com/jotaen/klog/releases/download/v3.1/klog-mac.zip"
+  url "https://github.com/jotaen/klog/releases/download/v3.1/klog-mac-intel.zip"
   sha256 "9739a961b5e42c7fa538108074dc991ff31e5fb5411a4324083e1651b051fd71"
   license "MIT"
   def install
