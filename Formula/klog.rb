fb9ad02363321a97e7d3777d648bc42eab36d0ad
Benjamin Bauer <benjaminf.bauer@gmail.com>
2021-12-01 12:50:48 +0100

    Update klog.rb
    the checksum is not correct

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -2,7 +2,7 @@ class Klog < Formula
   desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
   url "https://github.com/jotaen/klog/releases/download/v3.2/klog-mac-intel.zip"
-  sha256 "fd220cf07b705f9e1a5a638dc706ea9d844d38fc9e386ac7634218c4151ed143"
+  sha256 "4a91314cf1fbf0a6b9070263d4b791e9df2c050b25b5755f3821329619ac6a84"
   license "MIT"
   def install
     bin.install "klog"
