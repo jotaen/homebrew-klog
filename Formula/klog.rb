1913188c6bede5436e1e3d0fb3cd1de82f94749a
Jan Heuermann <jan@jotaen.net>
2022-03-21 15:30:43 +0100

    v4.0

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,8 +1,8 @@
 class Klog < Formula
   desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
-  url "https://github.com/jotaen/klog/releases/download/v3.3/klog-mac-intel.zip"
-  sha256 "02dfbcdf67d6385a13be6e8dbbf439cd62dc6a0973b3257ff6356c2a324e187d"
+  url "https://github.com/jotaen/klog/releases/download/v4.0/klog-mac-intel.zip"
+  sha256 "2b5694f0e0aecc66eec0ecf2bd360a261faaeaab0bfeb8bb8d1838224834d71d"
   license "MIT"
   def install
     bin.install "klog"
