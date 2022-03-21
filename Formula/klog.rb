8c24d2917fa11c88eb712fd5a56de9669ab91a0c
Jan Heuermann <jan@jotaen.net>
2022-03-21 15:37:17 +0100

    Update checksum

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -2,7 +2,7 @@ class Klog < Formula
   desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
   url "https://github.com/jotaen/klog/releases/download/v4.0/klog-mac-intel.zip"
-  sha256 "2b5694f0e0aecc66eec0ecf2bd360a261faaeaab0bfeb8bb8d1838224834d71d"
+  sha256 "24f3a0570ddc2f93f383fc3e6a585823c6d6749144051e7bcc0f51184374f0c3"
   license "MIT"
   def install
     bin.install "klog"
