79f653275e1f90ce954da3ea410d5777392b7b0f
Vladimirs Nordholm <v@vladde.net>
2022-04-19 12:10:05 +0200

    fix: use correct v5 release link

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,7 +1,7 @@
 class Klog < Formula
   desc "Time tracking CLI with plain-text files"
   homepage "https://github.com/jotaen/klog"
-  url "https://github.com/jotaen/klog/releases/download/5.0/klog-mac-intel.zip"
+  url "https://github.com/jotaen/klog/releases/download/v5.0/klog-mac-intel.zip"
   sha256 "2ca9b3f164a2056a1997d3355dd7b2833284f0ecc685d450dac42611f7bb6470"
   license "MIT"
   def install
