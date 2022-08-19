6b946934900b50d6f1699ecaeb38d50104f3a585
Jan Heuermann <jan@jotaen.net>
2022-08-19 20:36:30 +0200

    Replace double-quotes with single-quotes

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,10 +1,10 @@
 class Klog < Formula
-  desc "Time tracking CLI with plain-text files"
-  homepage "https://github.com/jotaen/klog"
-  url "https://github.com/jotaen/klog/releases/download/v5.1/klog-mac-intel.zip"
-  sha256 "6c43f3a882d20fc44d2dd5773d3cfb500009d102dbfa050580690773dc1a933e"
-  license "MIT"
+  desc 'Time tracking CLI with plain-text files'
+  homepage 'https://github.com/jotaen/klog'
+  url 'https://github.com/jotaen/klog/releases/download/v5.1/klog-mac-intel.zip'
+  sha256 '6c43f3a882d20fc44d2dd5773d3cfb500009d102dbfa050580690773dc1a933e'
+  license 'MIT'
   def install
-    bin.install "klog"
+    bin.install 'klog'
   end
 end
