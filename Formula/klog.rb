f41f68e8bd6eb9d74beeef704585b8469294ade2
Benjamin Bauer <benjaminf.bauer@gmail.com>
2023-01-21 14:03:43 +0000

    added support for installing arm64 version

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -1,9 +1,18 @@
 class Klog < Formula
   desc 'Time tracking CLI with plain-text files'
   homepage 'https://github.com/jotaen/klog'
-  url 'https://github.com/jotaen/klog/releases/download/v5.4/klog-mac-intel.zip'
-  sha256 '0b5c2b9fa900f8f1b25511ca6c079272720bfd9667787e25ef518615c568bf13'
   license 'MIT'
+
+  if Hardware::CPU.intel?
+    url 'https://github.com/jotaen/klog/releases/download/v5.4/klog-mac-intel.zip'
+    sha256 '0b5c2b9fa900f8f1b25511ca6c079272720bfd9667787e25ef518615c568bf13'
+  elsif Hardware::CPU.arm?
+    url 'https://github.com/jotaen/klog/releases/download/v5.4/klog-mac-arm.zip'
+    sha256 '27fff0633aa7e1740ae031875c8392a25b15a4c2e0fae728ed38772688777b09'
+  else
+    raise 'unexpected CPU'
+  end
+
   def install
     bin.install 'klog'
   end
--- generate-formula.sh
+++ generate-formula.sh
@@ -6,20 +6,33 @@ sudo apt update && sudo apt install -y curl jq
 
 echo 'Gathering info...'
 
-SHASUM="$(curl -sL https://github.com/jotaen/klog/releases/download/latest/klog-mac-intel.zip | sha256sum | head -c 64)"
-
 VERSION="$(curl --silent https://api.github.com/repos/jotaen/klog/releases/latest | jq -r '.name')"
+URL_INTEL="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-intel.zip"
+URL_ARM="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-arm.zip"
 
-echo "Checksum: ${SHASUM}"
+SHASUM_INTEL="$(curl -sL ${URL_INTEL} | sha256sum | head -c 64)"
+SHASUM_ARM="$(curl -sL ${URL_ARM} | sha256sum | head -c 64)"
+
+echo "Checksum Intel: ${SHASUM_INTEL}"
+echo "Checksum ARM: ${SHASUM_ARM}"
 echo "Version: ${VERSION}"
 
 echo 'Writing formula...'
 echo "class Klog < Formula
   desc 'Time tracking CLI with plain-text files'
   homepage 'https://github.com/jotaen/klog'
-  url 'https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-intel.zip'
-  sha256 '${SHASUM}'
   license 'MIT'
+
+  if Hardware::CPU.intel?
+    url '${URL_INTEL}'
+    sha256 '${SHASUM_INTEL}'
+  elsif Hardware::CPU.arm?
+    url '${URL_ARM}'
+    sha256 '${SHASUM_ARM}'
+  else
+    raise 'unexpected CPU'
+  end
+
   def install
     bin.install 'klog'
   end
