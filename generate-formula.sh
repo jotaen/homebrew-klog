6aa71b0a010ef5c0c980399f02e7dacf04b448d1
Benjamin Bauer <benjaminf.bauer@gmail.com>
2023-01-22 16:29:57 +0000

    make generator fail early
    pipefail: fail script when a command in a pipe fails
    move "debug" message to the matching command
    make curl fail when there is a problem
    make jq fail when there is a problem
    DRY CURL_FLAGS
    reverted to use sudo for apt-get
    -> (https://github.com/jotaen/homebrew-klog/pull/5#issuecomment-1399537260)

--- generate-formula.sh
+++ generate-formula.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 if [[ "$1" == "--install" ]]; then
   echo 'Installing script dependencies...'
-  apt-get update
-  ! command -v curl >/dev/null && apt-get install -y curl
-  ! command -v jq >/dev/null && apt-get install -y jq
+  sudo apt-get update
+  ! command -v curl >/dev/null && sudo apt-get install -y curl
+  ! command -v jq >/dev/null && sudo apt-get install -y jq
 fi
+
+CURL_FLAGS="--fail --silent --location --show-error"
 
 echo 'Gathering info...'
 
-VERSION="$(curl --silent https://api.github.com/repos/jotaen/klog/releases/latest | jq -r '.name')"
+VERSION="$(curl ${CURL_FLAGS} https://api.github.com/repos/jotaen/klog/releases/latest | jq -e -r '.name')"
+echo "retrieved version: ${VERSION}"
+
 URL_INTEL="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-intel.zip"
 URL_ARM="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-arm.zip"
 
-SHASUM_INTEL="$(curl -sL "${URL_INTEL}" | sha256sum | head -c 64)"
-SHASUM_ARM="$(curl -sL "${URL_ARM}" | sha256sum | head -c 64)"
+SHASUM_INTEL="$(curl ${CURL_FLAGS} "${URL_INTEL}" | sha256sum | head -c 64)"
+echo "retrieved checksum Intel: ${SHASUM_INTEL}"
 
-echo "Checksum Intel: ${SHASUM_INTEL}"
-echo "Checksum ARM: ${SHASUM_ARM}"
-echo "Version: ${VERSION}"
+SHASUM_ARM="$(curl ${CURL_FLAGS} "${URL_ARM}" | sha256sum | head -c 64)"
+echo "retrieved checksum ARM: ${SHASUM_ARM}"
 
 echo 'Writing formula...'
 echo "class Klog < Formula
