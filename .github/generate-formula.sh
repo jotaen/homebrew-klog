9a7abc57291b7588ffadd2b792dae139dfa441c4
Jan Heuermann <jan@jotaen.net>
2023-01-22 18:24:43 +0100

    Use bash array to store flags

--- .github/generate-formula.sh
+++ .github/generate-formula.sh
@@ -10,20 +10,20 @@ if [[ "$1" == "--install" ]]; then
   ! command -v jq >/dev/null && sudo apt-get install -y jq
 fi
 
-CURL_FLAGS="--fail --silent --location --show-error"
+readonly CURL_FLAGS=(--fail --silent --location --show-error)
 
 echo 'Gathering info...'
 
-VERSION="$(curl ${CURL_FLAGS} https://api.github.com/repos/jotaen/klog/releases/latest | jq -e -r '.name')"
+VERSION="$(curl "${CURL_FLAGS[@]}" https://api.github.com/repos/jotaen/klog/releases/latest | jq -e -r '.name')"
 echo "retrieved version: ${VERSION}"
 
 URL_INTEL="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-intel.zip"
 URL_ARM="https://github.com/jotaen/klog/releases/download/${VERSION}/klog-mac-arm.zip"
 
-SHASUM_INTEL="$(curl ${CURL_FLAGS} "${URL_INTEL}" | sha256sum | head -c 64)"
+SHASUM_INTEL="$(curl "${CURL_FLAGS[@]}" "${URL_INTEL}" | sha256sum | head -c 64)"
 echo "retrieved checksum Intel: ${SHASUM_INTEL}"
 
-SHASUM_ARM="$(curl ${CURL_FLAGS} "${URL_ARM}" | sha256sum | head -c 64)"
+SHASUM_ARM="$(curl "${CURL_FLAGS[@]}" "${URL_ARM}" | sha256sum | head -c 64)"
 echo "retrieved checksum ARM: ${SHASUM_ARM}"
 
 echo 'Writing formula...'
