4fa410c797e4792dd315c0406300ff6761dfc1c5
Jan Heuermann <jan@jotaen.net>
2024-03-03 21:51:28 +0100

    Update klog.rb

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -13,7 +13,7 @@ class Klog < Formula
     raise 'unexpected CPU'
   end
 
-  deprecate! date: "2024-03-03", because: "The jotaen/klog tap is no longer maintained and does not provide access to the latest klog releases anymore. Please see the call-for-maintainers at https://github.com/jotaen/homebrew-klog/issues/8"
+  deprecate! date: "2024-03-03", because: "The jotaen/klog tap is no longer maintained and does not provide access to the latest klog releases anymore. It will be deleted at some point in the future. Please see the call-for-maintainers at https://github.com/jotaen/homebrew-klog/issues/8"
 
   def install
     bin.install 'klog'
