89713f5e6e8bbf87a6db62a050e4259baee8f4d7
Jan Heuermann <jan@jotaen.net>
2024-03-30 09:25:17 +0100

    Invalidate download

--- Formula/klog.rb
+++ Formula/klog.rb
@@ -4,11 +4,11 @@ class Klog < Formula
   license 'MIT'
 
   if Hardware::CPU.intel?
-    url 'https://github.com/jotaen/klog/releases/download/v6.2/klog-mac-intel.zip'
-    sha256 '9808a7ad160788f05b9ba3cabc6ea55919911accab4b554c13df80b94aac2658'
+    url 'https://github.com/jotaen/klog/releases/download/v6.3/klog-mac-intel.zip'
+    sha256 '0000000000000000000000000000000000000000000000000000000000000000'
   elsif Hardware::CPU.arm?
-    url 'https://github.com/jotaen/klog/releases/download/v6.2/klog-mac-arm.zip'
-    sha256 'f3242c4297b24a4ac3bfd2cdf2fbeac075eb7972c92d4bd2555868896eae4b17'
+    url 'https://github.com/jotaen/klog/releases/download/v6.3/klog-mac-arm.zip'
+    sha256 '0000000000000000000000000000000000000000000000000000000000000000'
   else
     raise 'unexpected CPU'
   end
